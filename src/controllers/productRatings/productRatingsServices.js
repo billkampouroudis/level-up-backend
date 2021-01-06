33602f27c2d57203f5b1e40d171fe9cbe7073a40
Bill Kampouroudis <v.kampouroudis@gmail.com>
2021-01-06 21:21:01 +0200

    Fix create product rating logic

--- src/controllers/productRatings/productRatingsServices.js
+++ src/controllers/productRatings/productRatingsServices.js
@@ -1,12 +1,33 @@
 import { models } from '../../models';
 import { createSchema, listSchema } from './validation';
 import { addPagination } from '../../utils/misc/pagination';
+import { op } from '../../config/sequelize';
+import { ForbiddenError } from '../../constants/errors';
 
 export const createProductRatingService = async (data = {}) => {
-  const { ProductRating } = models;
+  const { ProductRating, OrderItem, Order } = models;
+  const { productId, userId } = data;
 
   try {
     await createSchema.validateAsync(data);
+
+    // Find if the user has ordered the given product.
+    const orderItem = await OrderItem.findOne({
+      where: { productId: productId },
+      include: [
+        {
+          model: Order,
+          where: { userId: userId, status: { [op.not]: 'in_cart' } }
+        }
+      ]
+    });
+
+    // Create rating only if user has bought the given product.
+    if (!orderItem) {
+      throw new ForbiddenError(
+        'User should have bought the given product in order to add a rating'
+      );
+    }
 
     const result = await ProductRating.create(data);
 
