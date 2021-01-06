6d18b6af5057fea047b807f3ffa829d35a0072af
Bill Kampouroudis <v.kampouroudis@gmail.com>
2021-01-06 23:36:46 +0200

    Fix eslint warnings

--- src/controllers/products/index.js
+++ src/controllers/products/index.js
@@ -1,11 +1,6 @@
 import STATUS from '../../constants/statusCodes';
 import { successResponse, errorResponse } from '../../utils/response';
-import {
-  BadRequestError,
-  UnprocessableEntityError,
-  NotFoundError,
-  InternalServerError
-} from '../../constants/errors';
+import { NotFoundError } from '../../constants/errors';
 import { models } from '../../models';
 import {
   createSchema,
@@ -16,7 +11,6 @@ import {
 import get from '../../utils/misc/get';
 import jwt_decode from 'jwt-decode';
 import findError from '../../utils/misc/errorHandling';
-import { sequelize } from '../../config/sequelize';
 
 export async function createProduct(req, res) {
   try {
@@ -40,7 +34,7 @@ export async function createProduct(req, res) {
 export async function getProduct(req, res) {
   try {
     const { productId } = req.params;
-    const { Product, Store, FavoriteProduct, ProductRating } = models;
+    const { Product, Store, FavoriteProduct } = models;
 
     await getSchema.validateAsync({ productId: parseInt(productId) });
 
